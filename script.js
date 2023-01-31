63db885f48db428476f75e62cd3b6f5b02472b9b
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-31 16:38:11 +0400

    palindrome checker solved

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str)
+{
+    let remChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
+    // Reverse remChar for comparison
+    let checkPalindrome = remChar.split('').reverse().join('');
+
+    // Check to see if str is a Palindrome
+    if(remChar === checkPalindrome)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
+
 module.exports = palindrome
